cdbb8bf0c1cd269b51f8b6406de1ff840c9428f8
webblyhm@gmail.com <holuyemo@gmail.com>
2026-09-03 16:05:22 +0100

    Use Rob West logo for site icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -92,7 +92,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
